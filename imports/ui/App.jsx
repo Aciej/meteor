7de161831a89270fc3e3bd8ad82f5400d13b737c
Andrzej <acieej@gmail.com>
2017-08-08 11:54:48 +0200

    eslint changes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
- 
-import Task from './Task.jsx';
- 
+
+import Task from './Task';
+
 // App component - represents the whole app
 export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
+  constructor() {
+    super();
+    this.state = {
+      tasks: [
+        { id: 1, text: 'This is task 1' },
+        { id: 2, text: 'This is task 2' },
+        { id: 3, text: 'This is task 3' },
+      ],
+    };
   }
-
   renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
+    return this.state.tasks.map(task => (
+      <Task key={task.id} task={task} />
     ));
   }
- 
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
         </header>
- 
+
         <ul>
           {this.renderTasks()}
         </ul>
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,16 +1,14 @@
-import React, { Component, PropTypes } from 'react';
- 
+import React, { PropTypes } from 'react';
+
 // Task component - represents a single todo item
-export default class Task extends Component {
-  render() {
-    return (
-      <li>{this.props.task.text}</li>
-    );
-  }
-}
- 
+const Task = ({ task }) => (
+  <li>{task.text}</li>
+);
+
 Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
-  task: PropTypes.object.isRequired,
+  task: PropTypes.shape({ text: {} }).isRequired,
 };
+
+export default Task;
